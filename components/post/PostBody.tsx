c45916758b6997a15523d987145bf2cc83958b1d
Michał Liebner <liebner15@gmail.com>
2023-05-15 22:04:42 +0200

    fix body

--- components/post/PostBody.tsx
+++ components/post/PostBody.tsx
@@ -1,7 +1,7 @@
 export function PostBody({ content }: { content: string }) {
   return (
-    <section className="mdx fade-in prose prose-invert col-start-1 row-start-2 mx-auto w-full pb-16">
-      <article>{content}</article>
+    <section className="fade-in col-start-1 row-start-2 mx-auto w-full pb-16">
+      <article className="mdx prose prose-invert w-full">{content}</article>
     </section>
   );
 }
