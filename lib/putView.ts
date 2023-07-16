8f3387850712f5ce3e3fbea99cc398bccaa35eb4
Michał Liebner <liebner15@gmail.com>
2023-07-16 22:09:15 +0200

    maybe this

--- lib/putView.ts
+++ lib/putView.ts
@@ -1,6 +1,16 @@
-export const putView = async (slug: string) => {
+export const putView = (slug: string) => {
   try {
-    console.log('a');
+    fetch(
+      `${
+        process.env.NODE_ENV === 'production'
+          ? 'https://personal-website-optimized.vercel.app/'
+          : 'http://localhost:3000'
+      }/api/posts/${slug}`,
+      {
+        method: 'PUT',
+        next: { revalidate: 0 },
+      }
+    );
   } catch (e) {
     console.log(e);
   }
