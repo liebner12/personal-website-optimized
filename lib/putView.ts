acacc4c2585bfbaf75bd8169449defa3cb0ff52d
Michał Liebner <liebner15@gmail.com>
2023-07-17 16:28:17 +0200

    why

--- lib/putView.ts
+++ lib/putView.ts
@@ -1,15 +1,6 @@
 export const putView = async (slug: string) => {
   try {
-    // fetch(
-    //   `${
-    //     process.env.NODE_ENV === 'production'
-    //       ? 'https://personal-website-optimized.vercel.app/'
-    //       : 'http://localhost:3000'
-    //   }/api/posts/${slug}`,
-    //   {
-    //     method: 'PUT',
-    //   }
-    // );
+    console.log(slug);
   } catch (e) {
     console.log(e);
   }
