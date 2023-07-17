70bc4c652f473e09c98d47046496879947f0e15e
Michał Liebner <liebner15@gmail.com>
2023-07-17 21:36:50 +0200

    common

--- components/PageView.tsx
+++ components/PageView.tsx
@@ -1,10 +1,9 @@
 'use client';
 import { useEffect } from 'react';
-import { putView } from 'lib/putView';
 
 export const PageView = ({ slug }: { slug: string }) => {
   useEffect(() => {
-    putView(slug);
+    console.log('');
   }, [slug]);
   return <div></div>;
 };
