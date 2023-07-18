9951ac14e702f296fe6997dba3838949db559a39
Michał Liebner <liebner15@gmail.com>
2023-07-18 20:24:55 +0200

    please work

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
-  },
   images: {
     domains: ['i.scdn.co', 'res.cloudinary.com'],
     minimumCacheTTL: 86400,
