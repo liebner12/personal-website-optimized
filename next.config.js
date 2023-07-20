297e4e9c522c3f4f043836e63c8698ca558ae5ff
Michał Liebner <liebner15@gmail.com>
2023-07-20 18:07:45 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,34 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['i.scdn.co', 'res.cloudinary.com'],
     minimumCacheTTL: 86400,
+  },
+  webpack: (config) => {
+    config.plugins.push(
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'data', 'blog'),
+            to: path.resolve(__dirname, '.next', 'data', 'blog'),
+            globOptions: {
+              ignore: ['**/*.mdx'],
+            },
+          },
+          {
+            from: path.resolve(__dirname, 'data', 'projects'),
+            to: path.resolve(__dirname, '.next', 'data', 'projects'),
+            globOptions: {
+              ignore: ['**/*.mdx'],
+            },
+          },
+        ],
+      })
+    );
+
+    return config;
   },
 };
